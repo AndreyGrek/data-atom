9bc7b2db15ab00d04c24e38fe04f7e9106510619
Andrey Grek <an.gee@icloud.com>
2021-03-12 23:22:10 +0300

    grid init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { Grid } from "react-virtualized";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
 import './App.css';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  search: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '97%',
+    },
+  },
+  cell: {
+    border: '1px solid black'
+  }
+}));
+
+const width = 20;
+const height = 1000;
+
+function generateData() {
+  const rows = [] as any;
+
+  for (let i = 0; i < height; i++) {
+    rows[i] = [];
+
+    for (let j = 0; j < width; j++) {
+      rows[i].push(`r: ${i}, c: ${j}`);
+    }
+  }
+
+  return rows;
+}
+
+function Table() {
+  const classes = useStyles();
+  const [search, setSearch] = React.useState('');
+
+  const filteredData = generateData().filter(row => row.some(cell => cell.toLowerCase().includes(search.toLowerCase())))
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const cellRenderer = (obj) => {
+    if (filteredData[obj.rowIndex]) {
+      return (
+        <div className={classes.cell} key={obj.key} style={obj.style} onDoubleClick={() => console.log(obj)}>
+          <div>{filteredData[obj.rowIndex][obj.columnIndex]}</div>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className={classes.search} noValidate autoComplete="off">
+        <TextField
+          id="outlined-name"
+          label="Search"
+          value={search}
+          onChange={handleSearch}
+          variant="outlined"
+        />
+      </form>
+        <Grid
+        cellRenderer={cellRenderer}
+        columnCount={width}
+        columnWidth={100}
+        height={500}
+        rowCount={filteredData.length}
+        rowHeight={30}
+        width={500}
+      />
     </div>
   );
 }
 
-export default App;
+export default Table;
